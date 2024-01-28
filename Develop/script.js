a04084d42a0fcbef4220e75158f947cdc70c6581
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-01-28 15:44:30 -0600

    wrote scaffolding jquery funct for the save buttons. it runs through each and affects the correct divs. just need to add the local storage functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
 // TO DO
-// 5. WHEN I click into a timeblock THEN I can enter an event
 // 6. WHEN I click the save button for that timeblock THEN the text for that event is saved in local storage
 // 7. WHEN I refresh the page THEN the saved events persist
 
@@ -42,12 +41,36 @@ const hourDivs = [
 // funct that runs the big code has to be in jquery so the page can't run it
 // til it hits the jquery call at the bottom of the html
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage
+  // TODO: 
+  // HINT:
+  //  - How might the id be useful when saving the description in local storage?
+
+  // BUTTON ONCLICKS
+
+  $(".saveBtn").click(function () {
+    // function runs on clicking the save button
+    for (let i = 0; i < hourDivs.length; i++) {
+      // for loop runs through each hour div
+
+      // runs on each parent hourDiv.
+      // gets location relative to the saveBtn via "this".
+      $(this).parent(hourDivs[i]).hide();
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
