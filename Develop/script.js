ec0bc81308386529d8b144b39df666b9ea0d584c
clendinning1 <149982688+clendinning1@users.noreply.github.com>
2024-01-28 12:24:03 -0600

    creating an array for each hour div. put the if statement inside a for loop that runs for each hour div. just need to get the makePast/etc functs to change the classes inside the for loop

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,22 @@
 
 // label this
 var currentTime = dayjs().format('MMM D, YYYY'); // current time
+var hour9 = $("#hour-9")
+var hour10 = $("hour-10")
+var hour11 = $("hour-11")
+var hour12 = $("hour-12")
+const relevantHour = [
+  hour9 = true,
+  hour10 = true,
+  hour11 = false,
+  hour12 = true
+];
+// no wait
+// if currentTime isBefore 9AM { hour-9 = past }
+// else if currentTime isAfter 9AM { hour-9 = future }
+// should i be doing it by hour instead?
+// try to make the relevantHour varry work, if not do this way
+
 
 
 
@@ -29,51 +45,65 @@ $(function () {
 
 
 
+
+  for (let i = 0; i < relevantHour.length; i++) {
+    // functions to set the class
+    // adds correct class for formatting
+    // rmvs other classes for resetting/updating when the time changes
+    function makePast() {
+      $(relevantHour[i]).removeClass("present");
+      $(relevantHour[i]).removeClass("future");
+      $(relevantHour[i]).addClass("past");
+    }
+
+    function makePresent() {
+      $(relevantHour).removeClass("past");
+      $(relevantHour).removeClass("future");
+      $(relevantHour).addClass("present");
+    }
+
+    function makeFuture() {
+      $(relevantHour).removeClass("past");
+      $(relevantHour).removeClass("present");
+      $(relevantHour).addClass("future");
+    }
+
+    // if statement that runs the shit
+    if (relevantHour[i] === true) {
+      console.log("BEFORE (true)");
+      makePast();
+    } else if (relevantHour[i] === false) {
+      console.log("AFTER (false)");
+      makeFuture();
+    } else {
+      console.log("ERROR");
+      makePresent();
+    }
+  }
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
-  // if we can get [timeblock time] to apply to all hours, then this code doesn't
-  // need to individually select, right????
 
-  var relevantHour
-  // which = an array of hour-9 thorugh hour-5 ??
-
-  // functions to set the class
-  // adds correct class for formatting
-  // rmvs other classes for resetting/updating when the time changes
-  function makePast() {
-    $(relevantHour).removeClass("present");
-    $(relevantHour).removeClass("future");
-    $(relevantHour).addClass("past");
-  }
-
-  function makePresent() {
-    $(relevantHour).removeClass("past");
-    $(relevantHour).removeClass("future");
-    $(relevantHour).addClass("present");
-  }
-
-  function makeFuture() {
-    $(relevantHour).removeClass("past");
-    $(relevantHour).removeClass("present");
-    $(relevantHour).addClass("future");
-  }
+  
 
 
   // if statement that sets the class depending on the hour
-  if (dayjs(relevantHour).isBefore(currentTime, 'hour')) {
-    console.log("BEFORE");
-    makePast();
-  } else if (dayjs(relevantHour).isSame(currentTime, 'hour')) {
-    console.log("CURRENT");
-    makePresent();
-  } else if (dayjs(relevantHour).isAfter(currentTime, 'hour')) {
-    console.log("AFTER");
-    makeFuture();
-  }
+  //if (dayjs(relevantHour).isBefore(currentTime, 'hour')) {
+  //  console.log("BEFORE");
+  //  makePast();
+  //} else if (dayjs(relevantHour).isSame(currentTime, 'hour')) {
+  //  console.log("CURRENT");
+  //  makePresent();
+  //} else if (dayjs(relevantHour).isAfter(currentTime, 'hour')) {
+  //  console.log("AFTER");
+  //  makeFuture();
+  //}
 
 
 
